1a897d688166d90596789aac72c93efa5c7d6cc0
saurabh-v-eq <saurabh.varma-t@equestsolutions.net>
2025-06-05 18:26:53 +0530

    add banner, newsletter section and implement functionality to remove item from cart using AJAX

--- assets/ajax-addToCart.js
+++ assets/ajax-addToCart.js
@@ -17,33 +17,71 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const updateCartDrawerHTML = async () => {
-    const response = await fetch("/?section_id=cart-drawer");
-    const text = await response.text();
+    try {
+      const response = await fetch("/?section_id=cart-drawer");
+      const text = await response.text();
 
-    const tempDiv = document.createElement("div");
-    tempDiv.innerHTML = text;
+      const tempDiv = document.createElement("div");
+      tempDiv.innerHTML = text;
 
-    const newDrawer = tempDiv.querySelector("#cart-drawer");
+      const newDrawer = tempDiv.querySelector("#cart-drawer");
 
-    if (newDrawer) {
-      cartDrawer.innerHTML = newDrawer.innerHTML;
-      openCartDrawer();
+      if (newDrawer) {
+        cartDrawer.innerHTML = newDrawer.innerHTML;
+        openCartDrawer();
+        attachRemoveBtnEvents();
+      }
+    } catch (error) {
+      console.error("Error updating cart drawer:", error);
     }
+  };
+
+  const attachRemoveBtnEvents = () => {
+    const removeBtns = document.querySelectorAll(".remove-item-button");
+    removeBtns.forEach((btn) => {
+      btn.addEventListener("click", async (event) => {
+        event.preventDefault();
+
+        const lineItemIndex = btn.getAttribute("data-line");
+        try {
+          await fetch("/cart/change.js", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+              line: lineItemIndex,
+              quantity: 0,
+            }),
+          });
+
+          await updateCartDrawerHTML();
+        } catch (error) {
+          console.log("Error removing item from cart", error);
+        }
+      });
+    });
   };
 
   addToCartForms.forEach((form) => {
     form.addEventListener("submit", async (event) => {
       event.preventDefault();
+      try {
+        await fetch("/cart/add.js", {
+          method: "POST",
+          body: new FormData(form),
+          headers: {
+            Accept: "application/json",
+          },
+        });
 
-      await fetch("/cart/add.js", {
-        method: "POST",
-        body: new FormData(form),
-        headers: {
-          Accept: "application/json",
-        },
-      });
-
-      await updateCartDrawerHTML();
+        await updateCartDrawerHTML();
+      } catch (error) {
+        console.error("Error adding item to cart:", error);
+      }
     });
   });
+
+  attachRemoveBtnEvents();
 });
